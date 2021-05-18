b0dd2e5009042acbda62fb5fb84a94cb245cbe3d
vapbyblr <vapbyblr@gmail.com>
2021-05-18 20:37:01 +0300

    first message

--- HM1/src/Main.java
+++ HM1/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-
+        System.out.println("");
+        System.out.println("To be or not to be that is the question");
+        System.out.println("Whether 'tis nobler in the mind to suffer");
+        System.out.println("The slings and arrows of outrageous fortune");
+        System.out.println("Or to take arms against a sea of troubles");
+        System.out.println("And by opposing end them");
     }
 }
