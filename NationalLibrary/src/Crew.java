b5ec337a8c66a5c4bca9e3a42fab9b05959b0060
Aivan-Ky <iky7@live.com>
2021-07-05 18:19:12 +0300

    ver 1.4

--- NationalLibrary/src/Crew.java
+++ NationalLibrary/src/Crew.java
@@ -1,10 +1,26 @@
 import java.util.HashSet;
+import java.util.Iterator;
 
-public class Crew {
-    private String crewName;
+public class Crew extends Worker {
     private HashSet<Worker> crewSet;
+    private double financialProposal;
 
-    // метод расчёта финансового предложения
+    public Crew crewRandomizer() {
+        Crew crew = new Crew();
+        HashSet<Worker> workerHashSet = new HashSet<>();
+        int maxCrewMembers = 20;
+        int minCrewMembers = 10;
+        int randomMembersAmount = minCrewMembers + (int)(Math.random() * (maxCrewMembers - minCrewMembers));
+        for (int i = 0; i <= randomMembersAmount; i++) {
+            workerHashSet.add(new Worker().workerRandomizer());
+        }
+    }
+
+    public double proposalCalculator(Crew crew) {
+        Iterator crewIterator = crewSet.iterator();
+        while (crewIterator.hasNext()) {
+        }
+    }
 
 
 }
